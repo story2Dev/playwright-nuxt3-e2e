568c2053e26829f5cc2ccb84ee7cb04447461684
Mark AI <chanthavong_12@hotmail.com>
2023-06-10 11:38:47 +0700

    init

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        About
+        <nuxt-link to="/">home</nuxt-link> 
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        index
+        <nuxt-link to="/about">About</nuxt-link>         
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
